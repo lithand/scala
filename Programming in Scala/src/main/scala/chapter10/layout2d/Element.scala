d858c0e1b710b3c371c1d93c3d3c89b1a1faa1f1
Zhang, Kui (u1c341) <KZhang@geico.com>
2013-02-13 18:14:50 -0500

    more contents

--- Programming in Scala/src/main/scala/chapter10/layout2d/Element.scala
+++ Programming in Scala/src/main/scala/chapter10/layout2d/Element.scala
@@ -1,14 +1,18 @@
 package chapter10.layout2d
+import Element.elem
 
 abstract class Element {
-	def contents: Array[String]
-	def height: Int = contents.length
-	def width: Int = if (height == 0) 0 else contents(0).length
-	def above(that: Element): Element = new ArrayElement (this.contents ++ that.contents);
-	def beside(that: Element): Element = {
-			val contents = new Array[String] (this.contents.length)
-			for(i <- 0 until this.contents.length)
-			  contents(i) = this.contents(i)+that.contents(i)
-			new  ArrayElement(contents)
-	}
+  def contents: Array[String]
+  def height: Int = contents.length
+  def width: Int = if (height == 0) 0 else contents(0).length
+  def above(that: Element): Element = new ArrayElement(this.contents ++ that.contents)
+  def beside(that: Element): Element = elem(
+    (this.contents zip that.contents) map (a => a._1 + a._2))
+  override def toString = contents mkString "\n"
+}
+
+object Element {
+  def elem(contents: Array[String]): Element = new ArrayElement(contents)
+  def elem(chr: Char, width: Int, height: Int): Element = new UniformElement(chr, width, height)
+  def elem(line: String): Element = new LineElement(line)
 }
